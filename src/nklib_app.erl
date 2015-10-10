20bdbef5d39be24e8e08f19871860cea42a923db
Carlos González Florido <carlosj.gf@gmail.com>
2015-10-10 17:47:25 +0200

    Allow no jsx in path

--- src/nklib_app.erl
+++ src/nklib_app.erl
@@ -45,7 +45,7 @@ start() ->
 
 %% @private OTP standard start callback
 start(_Type, _Args) ->
-	{module, jsx} = code:ensure_loaded(jsx),
+	code:ensure_loaded(jsx),
 	code:ensure_loaded(jiffy),     % We can work without it
     nklib_sup:start_link().
 
